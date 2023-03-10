52b407af4acc15bdef46d7dd6d42d37a86ecea8d
Mario Figueiredo <mfigueiredo@repay.com>
2023-03-10 10:10:27 +0000

    Finalize

--- CryptoService.cs
+++ CryptoService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 
+using Org.BouncyCastle.Bcpg;
 using PgpCore;
 
 namespace PayoutEncryption;
@@ -8,14 +9,17 @@ namespace PayoutEncryption;
 public partial class CryptoService
 {
     [Flags]
-    public enum Operation
+    public enum KeyType
     {
-        Encrypt = 1,
-        Decrypt = 2
+        Public = 1,
+        Private = 2
     }
 
     private readonly PGP _crypto;
-    private readonly Operation _operation;
+    private readonly KeyType _keyType;
+
+    private bool _armor;
+    private bool _validate;
 
     public CryptoService(string publicKey, string privateKey)
     {
@@ -25,45 +29,56 @@ public partial class CryptoService
         }
 
         _crypto = new PGP(new EncryptionKeys(publicKey, privateKey, ""));
-        _operation = Operation.Encrypt | Operation.Decrypt;
+        _keyType = KeyType.Public | KeyType.Private;
+        Configure();
     }
 
-    public CryptoService(string key, Operation operation)
+    public CryptoService(string key, KeyType operation)
     {
         EncryptionKeys keys;
 
         switch (operation)
         {
-            case Operation.Decrypt:
+            case KeyType.Private:
                 keys = new EncryptionKeys(key, "");
-                _operation = Operation.Decrypt;
+                _keyType = KeyType.Private;
                 break;
             default:
                 keys = new EncryptionKeys(key);
-                _operation = Operation.Encrypt;
+                _keyType = KeyType.Public;
                 break;
         }
 
         _crypto = new PGP(keys);
+        Configure();
     }
 
     public void PgpEncrypt(string fileInput)
     {
-        if (!_operation.HasFlag(Operation.Encrypt))
+        if (!_keyType.HasFlag(KeyType.Public))
         {
             throw new InvalidOperationException("A valid public key is required for encryption.");
         }
 
-        _crypto.EncryptFile(new FileInfo(fileInput), new FileInfo($"{fileInput}.pgp"));
+        _crypto.EncryptFile(new FileInfo(fileInput), new FileInfo($"{fileInput}.pgp"), _armor, _validate);
     }
 
     public void PgpDecrypt(string fileInput)
     {
-        if (!_operation.HasFlag(Operation.Decrypt))
+        if (!_keyType.HasFlag(KeyType.Private))
         {
             throw new InvalidOperationException("A valid private key is required for decryption.");
         }
 
         _crypto.DecryptFile(new FileInfo(fileInput), new FileInfo($"dec.{fileInput}"));
+    }
+
+    private void Configure()
+    {
+        _crypto.SymmetricKeyAlgorithm = SymmetricKeyAlgorithmTag.Aes128;
+        _crypto.CompressionAlgorithm = CompressionAlgorithmTag.Zip;
+
+        _armor = false;
+        _validate = true;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,35 @@
-﻿using System;
+﻿// USAGE:
+// ------
+// 
+// 1. If you don't supply any arguments, the executable will look for a file names `testfile.txt` in the current path and encrypt it.
+//
+// 2. The -key parameter can have the values public or private, and will determine both what key is going to be used and what operation is going to be performed.
+//       - public : will encrypt the input file using the public key
+//       - private : will decrypt the input file using the private key
+//       - defaults to public
+//
+// 3. The -in parameter can contain the relative or full path to a file to be encrypted or decrypted.
+//       - defaults to `testfile.txt`
+//
+// 4. If you want to create and use your own key pair, start by generating the sample key pairs that Repay and Paypal will use through the exchange.
+//
+//    $ gpg --gen-key
+//
+//    I've used "PayPal paypal@paypal.com" as the user and email address to simulate encrypting a payout file with hyperwallet public key and Paypal decrypting it with their private key.
+//    When asked for a password leave it blank. There is no information on hyperwallet documentation that the decryption will require password sharing between Repay and Paypal. Note: You'll have to hit the button on the left of the confirmation window a few times.
+//
+//    You can check that the keys exist with:
+//
+//    $ gpg--list - keys
+//
+//    Now we need to export these keys to a file so we can simulate the encryption and decryption process based on SSM and SM parameters.
+//
+//    $ gpg --output privat.pgp --armor --export-secret-key Paypal
+//    $ gpg --output public.pgp--armor--export Paypal
+//
+//    You should now have 2 ascii files in the current folder which you can open and copy into the code below
+
+using System;
 
 namespace PayoutEncryption;
 
@@ -6,39 +37,43 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        string enc = @"-----BEGIN PGP PUBLIC KEY BLOCK-----
+        const string pub = "mDMEZAoJrhYJKwYBBAHaRw8BAQdAXJrbUduBAuhxZP1+aOgxIoUgbB+vuLSZDEu4\r\n8xRGpnq0GlBheXBhbCA8cGF5cGFsQHBheXBhbC5jb20+iJkEExYKAEEWIQRxmnkr\r\nxW7U+MN8+pupMRrTtHAyIQUCZAoJrgIbAwUJA8JnAAULCQgHAgIiAgYVCgkICwIE\r\nFgIDAQIeBwIXgAAKCRCpMRrTtHAyIeFXAPwMOVXnf1MxejUIynhalBv8djylXDSh\r\nPXzHHVAh8DzvJQEAlG+i3sEIuU2+rBiw0YkDSoJAUYEsz02EfSvZJ/+G5wa4OARk\r\nCgmuEgorBgEEAZdVAQUBAQdARzxsTgcJ1HosbK5st/NHrpJecvfwAEZSIb4Quh3a\r\nrDADAQgHiH4EGBYKACYWIQRxmnkrxW7U+MN8+pupMRrTtHAyIQUCZAoJrgIbDAUJ\r\nA8JnAAAKCRCpMRrTtHAyIWz4AP4rRYy+qczaWjLG8EMBAsSaOHpfZI0nJa2AIc/9\r\nSAyXCQEA4W9k/lPX5T5WPrY5wKgG81IPQd+rWJKczKOGAMVdSgY=\r\n=feC8";
+        const string priv = "lFgEZAoJrhYJKwYBBAHaRw8BAQdAXJrbUduBAuhxZP1+aOgxIoUgbB+vuLSZDEu4\r\n8xRGpnoAAQDCBScQX4WTpVIsmxnpiXqGAaQEyU5RUU07Wv+jt36OIA2ItBpQYXlw\r\nYWwgPHBheXBhbEBwYXlwYWwuY29tPoiZBBMWCgBBFiEEcZp5K8Vu1PjDfPqbqTEa\r\n07RwMiEFAmQKCa4CGwMFCQPCZwAFCwkIBwICIgIGFQoJCAsCBBYCAwECHgcCF4AA\r\nCgkQqTEa07RwMiHhVwD8DDlV539TMXo1CMp4WpQb/HY8pVw0oT18xx1QIfA87yUB\r\nAJRvot7BCLlNvqwYsNGJA0qCQFGBLM9NhH0r2Sf/hucGnF0EZAoJrhIKKwYBBAGX\r\nVQEFAQEHQEc8bE4HCdR6LGyubLfzR66SXnL38ABGUiG+ELod2qwwAwEIBwAA/3rM\r\nDMg3D8Hf4rFQ9xSyp2YroRu0eZsTj49v7luyicMoEWqIfgQYFgoAJhYhBHGaeSvF\r\nbtT4w3z6m6kxGtO0cDIhBQJkCgmuAhsMBQkDwmcAAAoJEKkxGtO0cDIhbPgA/itF\r\njL6pzNpaMsbwQwECxJo4el9kjSclrYAhz/1IDJcJAQDhb2T+U9flPlY+tjnAqAbz\r\nUg9B36tYkpzMo4YAxV1KBg==\r\n=YvOw";
 
-mDMEZAoJrhYJKwYBBAHaRw8BAQdAXJrbUduBAuhxZP1+aOgxIoUgbB+vuLSZDEu4
-8xRGpnq0GlBheXBhbCA8cGF5cGFsQHBheXBhbC5jb20+iJkEExYKAEEWIQRxmnkr
-xW7U+MN8+pupMRrTtHAyIQUCZAoJrgIbAwUJA8JnAAULCQgHAgIiAgYVCgkICwIE
-FgIDAQIeBwIXgAAKCRCpMRrTtHAyIeFXAPwMOVXnf1MxejUIynhalBv8djylXDSh
-PXzHHVAh8DzvJQEAlG+i3sEIuU2+rBiw0YkDSoJAUYEsz02EfSvZJ/+G5wa4OARk
-CgmuEgorBgEEAZdVAQUBAQdARzxsTgcJ1HosbK5st/NHrpJecvfwAEZSIb4Quh3a
-rDADAQgHiH4EGBYKACYWIQRxmnkrxW7U+MN8+pupMRrTtHAyIQUCZAoJrgIbDAUJ
-A8JnAAAKCRCpMRrTtHAyIWz4AP4rRYy+qczaWjLG8EMBAsSaOHpfZI0nJa2AIc/9
-SAyXCQEA4W9k/lPX5T5WPrY5wKgG81IPQd+rWJKczKOGAMVdSgY=
-=feC8
------END PGP PUBLIC KEY BLOCK-----";
+        string key = pub;
+        string inFile = "testfile.txt";
+        CryptoService.KeyType keyType = CryptoService.KeyType.Public;
 
-        string dec = @"-----BEGIN PGP PRIVATE KEY BLOCK-----
+        for (int i = 0; i < args.Length; i++)
+        {
+            switch (args[i])
+            {
+                case "-key":
+                    var _ = Enum.TryParse(args[++i], ignoreCase: true, out keyType);
+                    key = (keyType == CryptoService.KeyType.Public) ? pub : priv;
+                    break;
+                case "-in":
+                    inFile = args[++i];
+                    if (string.IsNullOrWhiteSpace(inFile))
+                    {
+                        throw new ArgumentNullException("-in", "You must provide the path to an input file.");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
 
-lFgEZAoJrhYJKwYBBAHaRw8BAQdAXJrbUduBAuhxZP1+aOgxIoUgbB+vuLSZDEu4
-8xRGpnoAAQDCBScQX4WTpVIsmxnpiXqGAaQEyU5RUU07Wv+jt36OIA2ItBpQYXlw
-YWwgPHBheXBhbEBwYXlwYWwuY29tPoiZBBMWCgBBFiEEcZp5K8Vu1PjDfPqbqTEa
-07RwMiEFAmQKCa4CGwMFCQPCZwAFCwkIBwICIgIGFQoJCAsCBBYCAwECHgcCF4AA
-CgkQqTEa07RwMiHhVwD8DDlV539TMXo1CMp4WpQb/HY8pVw0oT18xx1QIfA87yUB
-AJRvot7BCLlNvqwYsNGJA0qCQFGBLM9NhH0r2Sf/hucGnF0EZAoJrhIKKwYBBAGX
-VQEFAQEHQEc8bE4HCdR6LGyubLfzR66SXnL38ABGUiG+ELod2qwwAwEIBwAA/3rM
-DMg3D8Hf4rFQ9xSyp2YroRu0eZsTj49v7luyicMoEWqIfgQYFgoAJhYhBHGaeSvF
-btT4w3z6m6kxGtO0cDIhBQJkCgmuAhsMBQkDwmcAAAoJEKkxGtO0cDIhbPgA/itF
-jL6pzNpaMsbwQwECxJo4el9kjSclrYAhz/1IDJcJAQDhb2T+U9flPlY+tjnAqAbz
-Ug9B36tYkpzMo4YAxV1KBg==
-=YvOw
------END PGP PRIVATE KEY BLOCK-----";
+        CryptoService crypto = new(key, keyType); // defaults to public key and ancryption
 
-        CryptoService crypto = new(enc, dec);
-
-        crypto.PgpEncrypt("testfile.txt");
-        crypto.PgpDecrypt("testfile.txt.pgp");
+        switch (keyType)
+        {
+            case CryptoService.KeyType.Private:
+                crypto.PgpDecrypt(inFile);
+                break;
+            default:
+                crypto.PgpEncrypt(inFile);
+                break;
+        }
     }
 }
